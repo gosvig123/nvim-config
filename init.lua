14d2dc9ea7fc3d603293ff000cd4e7009d88121c
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2024-09-14 16:35:23 +0200

    nvim update

--- init.lua
+++ init.lua
@@ -1,220 +1,302 @@
--- Check if running inside VSCode
-if vim.g.vscode then
-  -- VSCode-specific configuration
-  vim.g.mapleader = " "
-  vim.g.maplocalleader = " "
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.g.python3_host_prog = '/Users/kristian/miniconda3/bin/python'
 
-  -- Basic options
-  vim.opt.clipboard = "unnamedplus"
-  vim.opt.ignorecase = true
-  vim.opt.smartcase = true
+-- Basic options
+vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.number = true
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.shiftwidth = 2        -- Size of an indent
+vim.opt.softtabstop = 2       -- Number of spaces tabs count for in insert mode
+vim.opt.tabstop = 2           -- Number of spaces tabs count for
+vim.opt.smartindent = true    -- Insert indents automatically
+vim.opt.wrap = false          -- Disable line wrap
+vim.opt.undofile = true       -- Enable persistent undo
+vim.opt.incsearch = true      -- Show search matches as you type
+vim.opt.termguicolors = true  -- Enable 24-bit RGB colors
+vim.opt.scrolloff = 8         -- Keep 8 lines above/below cursor when scrolling
+vim.opt.sidescrolloff = 8     -- Keep 8 columns left/right of cursor when scrolling horizontally
+vim.opt.signcolumn = "yes"    -- Always show the signcolumne
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-  -- Folding options
-  vim.opt.foldmethod = "expr"
-  vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-  vim.opt.foldlevelstart = 99 -- Start with all folds open
-
-  -- VSCode-specific keymaps
-  local vscode = require("vscode-neovim")
-  local function vscode_keymap(mode, lhs, rhs, opts)
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, function()
-      vscode.call(rhs)
-    end, opts)
-  end
-
-  -- Navigation
-  vscode_keymap("n", "<C-j>", "workbench.action.navigateDown")
-  vscode_keymap("n", "<C-k>", "workbench.action.navigateUp")
-  vscode_keymap("n", "<C-h>", "workbench.action.navigateLeft")
-  vscode_keymap("n", "<C-l>", "workbench.action.navigateRight")
-
-  -- File and symbol search
-  vscode_keymap("n", "<leader>sf", "workbench.action.quickOpen")
-  vscode_keymap("n", "<leader>sg", "workbench.action.findInFiles")
-
-  -- Code actions
-  vscode_keymap("n", "<leader>ca", "editor.action.quickFix")
-  vscode_keymap("n", "<leader>rn", "editor.action.rename")
-
-  -- Comments
-  vscode_keymap("n", "gcc", "editor.action.commentLine")
-  vscode_keymap("v", "gc", "editor.action.commentLine")
-
-  -- Formatting
-  vscode_keymap("n", "<leader>f", "editor.action.formatDocument")
-
-  -- Which-key integration
-  vscode_keymap("n", "<leader>", "whichkey.show")
-
-  -- LSP-like functionality
-  vscode_keymap("n", "gd", "editor.action.revealDefinition")
-  vscode_keymap("n", "gr", "editor.action.goToReferences")
-  vscode_keymap("n", "gi", "editor.action.goToImplementation")
-  vscode_keymap("n", "K", "editor.action.showHover")
-
-  -- Folding keymaps
-  vscode_keymap("n", "za", "editor.toggleFold")
-  vscode_keymap("n", "zR", "editor.unfoldAll")
-  vscode_keymap("n", "zM", "editor.foldAll")
-  vscode_keymap("n", "zo", "editor.unfold")
-  vscode_keymap("n", "zc", "editor.fold")
-
-  -- Additional VSCode-specific keymaps
-  vscode_keymap("n", "<leader>e", "workbench.action.toggleSidebarVisibility")
-  vscode_keymap("n", "<leader>x", "workbench.action.closeActiveEditor")
-  vscode_keymap("n", "<leader>w", "workbench.action.files.save")
-  vscode_keymap("n", "<leader>q", "workbench.action.closeWindow")
-  vscode_keymap("n", "<leader>b", "workbench.action.toggleActivityBarVisibility")
-  vscode_keymap("n", "<leader>z", "workbench.action.toggleZenMode")
-
-  -- Plugins
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable",
-      lazypath,
-    })
-  end
-  vim.opt.rtp:prepend(lazypath)
-
-  require("lazy").setup({
-    -- Existing plugins
-    { "tpope/vim-surround" },
-    { "gcmt/wildfire.vim" },
-    {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-      end,
-      opts = {}
+-- Plugin specifications
+local plugins = {
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
     },
-    -- Tree-sitter
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        require("nvim-treesitter.configs").setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "python", "html", "css" },
-          highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-          },
+  },
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+          ensure_installed = { "lua", "vim", "vimdoc", "query" },
+          sync_install = false,
+          highlight = { enable = true },
           indent = { enable = true },
-          incremental_selection = { enable = true },
-          -- Folding
-          fold = { enable = true },
-        })
-      end,
+          autotag = { enable = true },
+          context_commentstring = { enable = true, enable_autocmd = true },
+      })
+    end
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+  {
+    "onsails/lspkind.nvim",
+    config = function()
+      require('lspkind').init()
+    end
+  },
+  {
+    "rafamadriz/friendly-snippets",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
     },
-    -- Additional syntax highlighting plugins
-    { "sheerun/vim-polyglot" },
-    {
-      "norcalli/nvim-colorizer.lua",
-      config = function()
-        require("colorizer").setup()
-      end,
-    },
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      opts = {},
-    },
-    -- Folding preview
-    {
-      "anuvyklack/pretty-fold.nvim",
-      config = function()
-        require("pretty-fold").setup()
+  },
+  {
+    "saadparwaiz1/cmp_luasnip",
+  },
+  -- Add Telescope to your plugins list
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+}
+
+-- Setup lazy.nvim
+require("lazy").setup(plugins)
+
+-- Basic VS Code configuration for TypeScript and Python development
+
+-- Autocompletion
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+-- Configure nvim-cmp
+local cmp = require('cmp')
+local lspkind = require('lspkind')
+local luasnip = require('luasnip')
+
+-- Load friendly-snippets
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
       end
-    },
-    {
-      "anuvyklack/fold-preview.nvim",
-      dependencies = "anuvyklack/keymap-amend.nvim",
-      config = function()
-        require("fold-preview").setup()
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
       end
-    },
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'copilot' },
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },  -- Add LuaSnip source
+    { name = 'vsnip' },
   }, {
-    performance = {
-      rtp = {
-        disabled_plugins = {
-          "gzip",
-          "matchit",
-          "matchparen",
-          "netrwPlugin",
-          "tarPlugin",
-          "tohtml",
-          "tutor",
-          "zipPlugin",
-        },
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = "symbol",
+      max_width = 50,
+      symbol_map = { Copilot = "" }
+    })
+  }
+})
+
+-- Set up lspconfig with cmp
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Enable native LSP
+local lspconfig = require('lspconfig')
+
+-- TypeScript
+local status, lspconfig = pcall(require, 'lspconfig')
+if not status then
+  print("Failed to load lspconfig")
+  return
+end
+
+-- Apply the fix for TypeScript server
+local server_name = "tsserver"
+if server_name == "tsserver" then
+  server_name = "ts_ls"
+end
+
+if lspconfig[server_name] then
+  local ts_status, _ = pcall(lspconfig[server_name].setup, {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+    init_options = {
+      preferences = {
+        importModuleSpecifierPreference = "relative",
       },
     },
+    single_file_support = false,
+    settings = {
+      typescript = {
+        inlayHints = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        }
+      },
+      javascript = {
+        inlayHints = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        }
+      }
+    }
   })
+  if not ts_status then
+    print("Failed to set up " .. server_name)
+  end
+else
+  print(server_name .. " not available in lspconfig")
+end
 
-  -- LSP Configuration
-  local servers = {
-    -- Python LSP
-    pyright = {},
-    
-    -- JavaScript/TypeScript LSP
-    tsserver = {},
+-- Python
+lspconfig.pyright.setup {
+  capabilities = capabilities
+}
 
-    -- Optional: Add more LSPs as needed
-    -- eslint = {},
-    -- pylsp = {}, -- An alternative to pyright
-  }
+-- Keybindings
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-  -- Ensure the servers above are installed
-  require('mason').setup()
-  require('mason-lspconfig').setup {
-    ensure_installed = vim.tbl_keys(servers),
-    handlers = {
-      function(server_name)
-        local server = servers[server_name] or {}
-        -- This sets up the LSP with default configurations
-        require('lspconfig')[server_name].setup(server)
-      end,
-    },
-  }
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Set up LSP keybindings
-  vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-      local opts = { buffer = ev.buf }
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    end,
-  })
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
 
-  -- Autocompletion setup
+-- Add on_attach to both language servers
+lspconfig.pyright.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+if vim.g.vscode then
+  -- VSCode extension
+  local vscode = require('vscode-neovim')
+
+  -- Autocompletion
+  vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+  -- Configure nvim-cmp
   local cmp = require('cmp')
+  local luasnip = require('luasnip')
+
+  -- Load friendly-snippets
+  require("luasnip.loaders.from_vscode").lazy_load()
+
   cmp.setup({
     snippet = {
       expand = function(args)
-        require('luasnip').lsp_expand(args.body)
+        luasnip.lsp_expand(args.body)
       end,
     },
     mapping = cmp.mapping.preset.insert({
@@ -223,22 +305,114 @@ if vim.g.vscode then
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
     }),
     sources = cmp.config.sources({
+      { name = 'vscode-neovim' },
+      { name = 'copilot' },
       { name = 'nvim_lsp' },
       { name = 'luasnip' },
-    }, {
       { name = 'buffer' },
     })
   })
 
-  -- Additional VSCode-specific settings
-  vim.g.vscode_snippets_enabled = true
-  vim.g.vscode_completion_enabled = true
-  vim.g.vscode_format_on_save = true
+  -- Set up lspconfig
+  local lspconfig = require('lspconfig')
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-  -- Defer loading of some functionality
-  vim.defer_fn(function()
-    -- Add any deferred loading here
-  end, 100)
+  -- Common on_attach function
+  local on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Add any other common setup here
+  end
+
+  -- Configure tsserver
+  lspconfig.tsserver.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+    init_options = {
+      preferences = {
+        importModuleSpecifierPreference = "relative",
+      },
+    },
+    single_file_support = false,
+    settings = {
+      typescript = {
+        inlayHints = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        }
+      },
+      javascript = {
+        inlayHints = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        }
+      }
+    }
+  }
+
+  -- Configure pyright
+  lspconfig.pyright.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          useLibraryCodeForTypes = true,
+          diagnosticMode = "workspace"
+        }
+      }
+    }
+  }
+
+  -- ... rest of your existing VSCode-specific configuration ...
 end
+
+-- Set up Telescope
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<space>f', telescope.find_files, {})
+vim.keymap.set('n', '<space>g', telescope.live_grep, {})
+vim.keymap.set('n', '<space>b', telescope.buffers, {})
+-- Add this new keymapping for searching in the current buffer
+vim.keymap.set('n', '<leader>/', function()
+  telescope.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+print("LSP configuration completed")
